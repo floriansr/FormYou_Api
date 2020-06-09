831f3d234449e7821a914708b880740f488da046
julienemo <juliette.nada@gmail.com>
2020-06-09 18:40:08 +0200

    [add] seeded and model rspec tests

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+require 'faker'
+
+PASSWORD = "111111".freeze
+
+puts "Creating fake administrators..."
+
+n = 1 
+3.times do
+  Administrator.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: "seededadmin#{n}@yopmail.com",
+    password: PASSWORD
+  )
+  n += 1
+end
+
+puts "#{Administrator.count} admin profiles created."
