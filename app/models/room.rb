2e75be4f081089dfc15a3042179ac6a607be49c4
julienemo <juliette.nada@gmail.com>
2020-06-10 16:36:54 +0200

    [add] room model

---
+++ app/models/room.rb
@@ -0,0 +1,7 @@
+class Room < ApplicationRecord
+
+  validates :name, presence: true, uniqueness: { case_sensitivity: false }
+  # TODO: we might want to have a method to return a string
+  # of available room for a certain time slot on a certain day
+
+end
