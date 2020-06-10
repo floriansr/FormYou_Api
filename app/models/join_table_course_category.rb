186ec83c024fe6b6b197d971249f5f4026168f78
Raphael Chappert <raphael.chappert@hotmail.fr>
2020-06-10 19:46:00 +0200

    edit category course and jointable models for validations and relations

--- app/models/join_table_course_category.rb
+++ app/models/join_table_course_category.rb
@@ -2,10 +2,7 @@ class JoinTableCourseCategory < ApplicationRecord
 
   belongs_to :course
   belongs_to :category
-<<<<<<< HEAD
-=======
   validates :course_id, uniqueness: { scope: :category_id,
                                       message: 'that relation already exists' }
 
->>>>>>> 43a2158... rubocop corrections
 end
